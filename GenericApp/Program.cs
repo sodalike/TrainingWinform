24f2bfe44b17c0e0e601c15ded497ecb41210bfd
sodalike <dks00990@naver.com>
2020-06-15 12:32:28 +0900

    ㅇㄹㄹㄹㄹㄹ

--- GenericApp/Program.cs
+++ GenericApp/Program.cs
@@ -1,16 +1,61 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Markup;
 
 namespace GenericApp
 {
+    public class SimpleGeneric<T>
+    {
+        private T[] values;
+        private int index;
+
+        public SimpleGeneric(int len)
+        {
+            values = new T[len];
+            index = 0;
+        }
+
+        public void Add(params T[] args)
+        {
+            foreach (T item in args)
+            {
+                values[index++] = item;
+            }
+        }
+
+        public void Print()
+        {
+            foreach (T item in values)
+            {
+                Console.Write(item + ", ");
+            }
+            Console.WriteLine();
+        }
+
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hell World");
+            SimpleGeneric<Int32> gInterger = new SimpleGeneric<int>(10);
+            SimpleGeneric<Double> gDouble = new SimpleGeneric<Double>(10);
+
+            gInterger.Add(1, 2);
+            gInterger.Add(1, 2, 3, 4, 5, 6, 7);
+            gInterger.Add(0);
+            gInterger.Print();
+
+            gDouble.Add(10.0, 20.0, 30.0);
+            gDouble.Print();
+
+
+
+
+
         }
     }
 }
